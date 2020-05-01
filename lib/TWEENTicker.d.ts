19f74373417d8827e257a995f0b7510162d13488
MasatoMakino <unagiinu.g@gmail.com>
2020-05-01 14:33:42 +0900

    update : dependencies.

--- lib/TWEENTicker.d.ts
+++ lib/TWEENTicker.d.ts
@@ -1,8 +1,8 @@
 export declare class TWEENTicker {
-  private static isStart;
-  static initialize(): void;
-  static start(): void;
-  private static update;
-  static stop(): void;
+    private static isStart;
+    static initialize(): void;
+    static start(): void;
+    private static update;
+    static stop(): void;
 }
 //# sourceMappingURL=TWEENTicker.d.ts.map
